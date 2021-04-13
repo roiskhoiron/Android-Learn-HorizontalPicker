8a4e45f6ef79199617b4ac674c43e70d073be451
Rois <rois.khoiron@gmail.com>
2021-04-13 09:02:26 +0700

    Final commit

---
+++ app/src/main/java/com/example/horizontalpicker/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.horizontalpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
+import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
+
+import org.joda.time.DateTime;
+
+public class MainActivity extends AppCompatActivity implements DatePickerListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        HorizontalPicker picker = findViewById(R.id.datePicker);
+        picker.setListener(this)
+                .setDays(120)
+                .setOffset(7)
+                .setDateSelectedColor(Color.DKGRAY)
+                .setDateSelectedTextColor(Color.WHITE)
+                .setMonthAndYearTextColor(Color.DKGRAY)
+                .setTodayButtonTextColor(getResources().getColor(R.color.colorPrimary))
+                .setTodayDateTextColor(getResources().getColor(R.color.colorPrimary))
+                .setTodayDateBackgroundColor(Color.GRAY)
+                .setUnselectedDayTextColor(Color.DKGRAY)
+                .setDayOfWeekTextColor(Color.DKGRAY)
+                .setUnselectedDayTextColor(getResources().getColor(R.color.primaryTextColor))
+                .showTodayButton(false)
+                .init();
+
+        picker.setBackgroundColor(Color.LTGRAY);
+        picker.setDate(new DateTime());
+    }
+
+    @Override
+    public void onDateSelected(DateTime dateSelected) {
+        Toast.makeText(this, dateSelected.toString(), Toast.LENGTH_SHORT).show();
+    }
+}
